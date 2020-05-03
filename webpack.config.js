6809ef70d3ccf84c37e6b742becd637b3ebfcd16
Alen <alechkr@gmail.com>
2020-05-03 17:19:13 +0200

    installed babel and made the adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,25 @@ const webpack = require('webpack');
 
 
 module.exports = {
-    mode: 'production',
-    entry: './src/client/index.js'
+    /* mode: 'production', */
+    entry: './src/client/index.js'  
+    
+    
+
+    
 };
+
+//If you do not write the following code in module.exports -> mode: 'production',
+//WARNING in configuration
+//The 'mode' option has not been set, webpack will fallback to 'production' for this value. Set 'mode' option to 'development' or 'production' to enable defaults for each environment.
+//You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode///
+
+module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
+}
