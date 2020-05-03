5f9db9472cd701228a6c07cdad695117b30a1511
Alen <alechkr@gmail.com>
2020-05-03 17:50:03 +0200

    changed some syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
-const path = require('path');
-const webpack = require('webpack');
-
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-    /* mode: 'production', */
-    entry: './src/client/index.js'  
-    
-    
-
-    
-};
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
 
 //If you do not write the following code in module.exports -> mode: 'production',
 //WARNING in configuration
 //The 'mode' option has not been set, webpack will fallback to 'production' for this value. Set 'mode' option to 'development' or 'production' to enable defaults for each environment.
 //You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode///
 
-module: {
-    rules: [
-            {
-        test: '/\.js$/',
-        exclude: /node_modules/,
-        loader: "babel-loader"
-            }
-    ]
-}
+
