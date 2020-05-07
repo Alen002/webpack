ee6ca22a74246580f4f3dbaece523d5be258a96f
Alen <alechkr@gmail.com>
2020-05-07 23:48:42 +0200

    scss loaders installed and implemented

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,13 +12,20 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
-        ]
+        
+        ],
+
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html"
         })
-    ]
+    ],
+    
 }
